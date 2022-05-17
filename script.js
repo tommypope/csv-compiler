0c0e04390c35d9847079417663525038a07988ff
tommypope <tom@tbook.attlocal.net>
2022-05-17 15:47:42 -0700

    init node and add modules to git ignore

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// script to combile records into master csv file
+// Script to combile records into master csv file
 //
-// fields:
+// Fields:
 // Source,First Name,Last Name,Affiliation,Email,Phone,Origin,Instagram
 //
-// 1. compile all csv files into main file
-// 2. sort file alphabetically by 'Last Name'
-// 3. check for multiple records with same 'First Name' and 'Last Name'
-//     -when multiple records with same 'First Name' and 'Last Name', combine records into 1
-//     -when combining records, if both have a populated field, prefer the last
+// 1. Compile all csv files into main file
+// 2. Sort file alphabetically by 'Last Name'
+// 3. Check for multiple records with same 'First Name' and 'Last Name'
+//     -When multiple records with same 'First Name' and 'Last Name', combine records into 1
+//     -When combining records, if both have a populated field, prefer the last
 //
 
 const fields = {
@@ -21,5 +21,5 @@ const fields = {
   'Instagram': []
 }
 
-// use fs.readfile fs.writefile?
-// something to convert csv into mutable json? papaparse
+// Use fs.readfile fs.writefile?
+// Something to convert csv into mutable json? papaparse
