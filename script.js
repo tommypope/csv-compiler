ee5a6ebc138acdc02441a4f3d79fe551719b8254
tommypope <tom@tbook.attlocal.net>
2022-05-17 10:14:20 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Script to combile records into master CSV file
+//
+// Source,First Name,Last Name,Affiliation,Email,Phone,Origin,Instagram
+//
+// 1. Compile all CSV files into main file
+// 2. Sort file alphabetically by Last Name
+// 3. Check for multiple records with same First Name & Last Name
+//     -When multiple records with same First Name & Last Name, combine records into 1
+//     -When combining records, if both have a populated field, prefer the last
+//
+
